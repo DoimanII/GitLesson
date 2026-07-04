ab119347a16d0e14bda4f57e9334021822b068b6
Doiman <88896022+DoimanII@users.noreply.github.com>
2026-07-04 20:20:24 +0700

    Updated just_fucn

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ def print_hi(name):
 
 def just_func():
     arr: list = [number for number in range(100+1)]
+    counter: int = 0
+
     for value in arr:
         print(value)
+        print(100 + value)
+
 
 if __name__ == '__main__':
     print_hi('Hi Git!')
