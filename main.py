399e0b962af04791c4ee3eeb73da99bca42ae0ca
Doiman <88896022+DoimanII@users.noreply.github.com>
2026-07-08 22:26:15 +0700

    Added convector from float to int

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def calculate_func(x, y):
     return (x*y)**0.5
 
+
+def from_float_to_int(value: float) -> int:
+    return int(value)
+
+
 if __name__ == '__main__':
-    print(calculate_func(5, 5))
+    result = calculate_func(5, 5)
+    result = from_float_to_int(result)
+    print(result)
