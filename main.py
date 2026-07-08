e7485d74f3586d5efbae62518fb29683361fcb85
Doiman <88896022+DoimanII@users.noreply.github.com>
2026-07-08 22:38:15 +0700

    Added some code

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def from_float_to_int(value: float) -> int:
     return int(value)
 
 
+def some_func() -> None:
+    print('some_func')
+
 if __name__ == '__main__':
     result = calculate_func(5, 5)
     result = from_float_to_int(result)
+    some_func()
     print(result)
